7bb53a3cc3a66e8980bcf1d37349605e8537cec3
gustavoarvani2003-code <gustavo.arvani2003@gmail.com>
2026-03-23 23:29:43 -0300

    adicionar e excluir

--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 let listElement = document.querySelector("#app ul");
 
 let tarefas = [];
 
-let novaTarefa = {
-    if()
+function renderTarefas(){
+    listElement.innerHTML = '';
+    tarefas.map((tarefa)=>{
+        
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(tarefa);
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+        let excluirText = document.createTextNode(" Excluir");
+        
+        let posicaoTarefa = tarefas.indexOf(tarefa)
+
+        linkElement.setAttribute("onclick",`excluirTarefa(${posicaoTarefa})`)
+
+        liElement.appendChild(tarefaText);
+        linkElement.appendChild(excluirText);
+        liElement.appendChild(linkElement);
+        listElement.appendChild(liElement);
+    })
 }
+
+function adicionarTarefas(){
+    
+    if(inputElement.value === ''){
+        alert("Digite uma tarefa")
+        return false
+    }else{
+        let novaTarefa = inputElement.value;
+        tarefas.push(novaTarefa);
+        renderTarefas();
+        inputElement.value = '';
+    }
+}
+
+function excluirTarefa(posicaoTarefa){
+    tarefas.splice(posicaoTarefa, 1);
+    renderTarefas();
+}
+
+buttonElement.onclick = adicionarTarefas;
+
+console.log(tarefas)
