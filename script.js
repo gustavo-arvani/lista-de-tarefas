2dae9d9f7b7d8e3cf40dc506036956b87df51c91
gustavoarvani2003-code <gustavo.arvani2003@gmail.com>
2026-03-24 22:26:30 -0300

    finalização

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 let listElement = document.querySelector("#app ul");
 
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas"))  || [];
+
+renderTarefas();
 
 function renderTarefas(){
     listElement.innerHTML = '';
@@ -11,17 +13,36 @@ function renderTarefas(){
         
         let liElement = document.createElement("li");
         let tarefaText = document.createTextNode(tarefa);
+
         let linkElement = document.createElement("a");
         linkElement.setAttribute("href", "#");
-        let excluirText = document.createTextNode(" Excluir");
+        let excluirText = document.createTextNode("❌ ");
+
+
+        let linkEditElement = document.createElement("a");
+        linkEditElement.setAttribute("href", "#");
+        let editText = document.createTextNode(" ✏️");
+
+        let divLinks = document.createElement("div");
+        divLinks.setAttribute("class", "acoes");
+
+        let divTarefa = document.createElement("div");
+        divTarefa.setAttribute("class", "tarefas");
+
         
-        let posicaoTarefa = tarefas.indexOf(tarefa)
+        let posicaoTarefa = tarefas.indexOf(tarefa);
 
-        linkElement.setAttribute("onclick",`excluirTarefa(${posicaoTarefa})`)
+        linkElement.setAttribute("onclick",`excluirTarefa(${posicaoTarefa})`);
+        linkEditElement.setAttribute("onclick", `editarTarefa(${posicaoTarefa})`);
 
-        liElement.appendChild(tarefaText);
+
         linkElement.appendChild(excluirText);
-        liElement.appendChild(linkElement);
+        linkEditElement.appendChild(editText);
+        divLinks.appendChild(linkElement);
+        divLinks.appendChild(linkEditElement);
+        divTarefa.append(tarefaText);
+        liElement.appendChild(divTarefa);
+        liElement.appendChild(divLinks);
         listElement.appendChild(liElement);
     })
 }
@@ -29,12 +50,13 @@ function renderTarefas(){
 function adicionarTarefas(){
     
     if(inputElement.value === ''){
-        alert("Digite uma tarefa")
+        alert("Digite uma tarefa");
         return false
     }else{
         let novaTarefa = inputElement.value;
         tarefas.push(novaTarefa);
         renderTarefas();
+        salvarDados();
         inputElement.value = '';
     }
 }
@@ -42,8 +64,24 @@ function adicionarTarefas(){
 function excluirTarefa(posicaoTarefa){
     tarefas.splice(posicaoTarefa, 1);
     renderTarefas();
+    salvarDados();
+}
+
+function editarTarefa(posicaoTarefa){
+   let novoValor = prompt("Digite a atualização da tarefa");
+   if(novoValor === '' || novoValor === null){
+    alert("Tarefa não atualizada");
+    return false;
+   }else{
+    tarefas[posicaoTarefa] = novoValor;
+    renderTarefas();
+    salvarDados();}
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas));
 }
 
 buttonElement.onclick = adicionarTarefas;
 
-console.log(tarefas)
+console.log(tarefas);
